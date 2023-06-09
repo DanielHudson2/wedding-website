ead46ad110cc2fa7e8f071ee8fca5d7febc06358
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2023-06-09 17:12:52 +0100

    Fix: remove unnecessary input

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -67,7 +67,6 @@ const RSVPPage = () => {
 				method="POST"
 				data-netlify="true"
 			>
-				<input type="hidden" name="form-name" value="rsvp-form" />
 				{people.map((person, index) => (
 				<div className="form-section" key={index}>
 					<h3>{person.firstName} {person.lastName}</h3>
