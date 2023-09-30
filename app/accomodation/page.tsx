47da355f9c042844499fa06e16f864932d4892af
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2023-09-30 17:48:05 +0100

    New: added accomodation page

---
+++ app/accomodation/page.tsx
@@ -0,0 +1,55 @@
+'use client';
+import Image from 'next/image'
+
+export default function OurVenue() {
+  return (
+    <main>
+      
+      <div className="width-container">
+
+        <div className="hoz-padding">
+          <h1 className='body-font'>Accomodation</h1>
+          <p>If you are planning on staying in the area while you are attending our wedding we've put together a bit of information on accomodation options for you.</p>
+        </div>
+
+        <div className='text-with-image'>
+
+          <div className='text-with-image__content fadeInLeft animate'>
+            <h2>Eastington Park</h2>
+            <p>We are really clever so we found a venue with plenty of accomodation so that if you'd like you can stay with us and you'll even get breakfast in the morning.</p>
+            <p>Eastington Park handles all the accomodation themselves so if you'd like to book then you can get in touch with them directly using <a href="tel:+441453827926">+44(0)1453 827926</a></p>
+            <p>You can find our more about their accomodation <a href="https://www.eastingtonpark.co.uk/accommodation/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+          </div>
+
+          <Image
+            src='/eastington.jpg'
+            width='680'
+            height='453'
+            alt='Eastington Park'
+            className='fadeInRight animate'
+          />
+          
+        </div>
+
+        <div className="grid hoz-padding">
+
+          <div className='fadeInLeft animate'>
+            <h3>Travelodge</h3>
+            <p>The next closest accomodation is Stonehouse Travelodge, it is a very short drive away so is perfect if you can't get a room in Eastington Park but still want to be close by.</p>
+            <p>You can find out more about this accommodation <a href="https://www.travelodge.co.uk/hotels/99/Stonehouse-hotel" target="_blank" rel="noopener noreferrer">here</a>.</p>
+          </div>
+
+          <div className='fadeInRight animate'>
+            <h3>Others</h3>
+            <p>We are sure there's plenty of other accomodation in the area you could find if those don't work for you, here is a <a href="https://www.google.com/search?q=hotels+in+stonehouse+gloucestershire" target="_blank" rel="noopener noreferrer">Google Search</a> to get you started.</p>
+          </div>
+
+        </div>
+
+        <br></br>
+
+      </div>
+
+    </main>
+  )
+}
--- app/our-venue/page.tsx
+++ app/our-venue/page.tsx
@@ -34,11 +34,11 @@ export default function OurVenue() {
           <h3>The Important Bits</h3>
           <ul>
             <li>Eastington Park has accomodation which you can find more about <a href="/accomodation">here</a>.</li>
-            <li>The address is Eastington Park, Gloucestershire, GL10 3RY you can get directions <a href="https://www.google.com/maps/place/Eastington+Park/@51.7496031,-2.3139865,17z/data=!3m1!4b1!4m6!3m5!1s0x487109f746679a5d:0xbf145f5aa0e00283!8m2!3d51.7495998!4d-2.3114116!16s%2Fg%2F1pp2x67g7?entry=ttu" target='_blank' rel='no-follow'>here</a>.</li>
+            <li>The address is Eastington Park, Gloucestershire, GL10 3RY you can get directions <a href="https://www.google.com/maps/place/Eastington+Park/@51.7496031,-2.3139865,17z/data=!3m1!4b1!4m6!3m5!1s0x487109f746679a5d:0xbf145f5aa0e00283!8m2!3d51.7495998!4d-2.3114116!16s%2Fg%2F1pp2x67g7?entry=ttu" target='_blank' rel="noopener noreferrer">here</a>.</li>
             <li>You are able to drive all the way to the front door for dropoffs but we ask that everyone uses the provided carpark which is signposted on the driveway.</li>
           </ul>
           <br></br>
-          <p>You can find out anything else you need by visting Eastington Park's website <a href="https://www.eastingtonpark.co.uk/" target='_blank' rel='no-follow'>www.eastingtonpark.co.uk</a></p>
+          <p>You can find out anything else you need by visting Eastington Park's website <a href="https://www.eastingtonpark.co.uk/" target='_blank' rel="noopener noreferrer">www.eastingtonpark.co.uk</a></p>
           <br></br>
         </div>
 
