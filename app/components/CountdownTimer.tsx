8f053bf1db6eca102ce48544d5dd310017e3c405
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2024-05-07 14:47:47 +0100

    Fix: countdown going backwards

--- app/components/CountdownTimer.tsx
+++ app/components/CountdownTimer.tsx
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react';
 
 const CountdownTimer = () => {
   const [countdown, setCountdown] = useState(0);
+  const [targetDatePassed, setTargetDatePassed] = useState(false);
 
   useEffect(() => {
-    const targetDate: Date = new Date('2024-04-11'); // Set your target date here
-    targetDate.setHours(14, 0, 0, 0);
+    const targetDate: Date = new Date('2024-04-11T14:00:00'); // Set your target date here
     const currentTime: Date = new Date();
 
-    const difference: number = targetDate.getTime() - currentTime.getTime();
-    const countdownInSeconds: number = Math.floor(difference / 1000);
-
-    setCountdown(countdownInSeconds);
-
-    const timer = setInterval(() => {
-      setCountdown((prevCountdown) => prevCountdown - 1);
-    }, 1000);
-
-    return () => clearInterval(timer);
+    if ( currentTime > targetDate ) {
+      setTargetDatePassed(true);
+    } else {
+      const difference: number = targetDate.getTime() - currentTime.getTime();
+      const countdownInSeconds: number = Math.floor(difference / 1000);
+  
+      setCountdown(countdownInSeconds);
+  
+      const timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+  
+      return () => clearInterval(timer);
+    }
   }, []);
+
+  if ( targetDatePassed ) {
+
+    return (
+      	<div className="countdown-timer">
+			We did it! we got married.
+		</div>
+    );
+
+  }
 
   const formatTime = (time: number) => {
     return time < 10 ? `${time}` : time;
